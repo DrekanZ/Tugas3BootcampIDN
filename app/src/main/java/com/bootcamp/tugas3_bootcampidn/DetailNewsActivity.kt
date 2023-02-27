312268ec37e9af43ee63ab8ca91fc4d5a3fda7a5
DrekanZ <drekanz21@gmail.com>
2023-02-27 13:08:58 +0700

    Intent

--- app/src/main/java/com/bootcamp/tugas3_bootcampidn/DetailNewsActivity.kt
+++ app/src/main/java/com/bootcamp/tugas3_bootcampidn/DetailNewsActivity.kt
@@ -2,8 +2,13 @@ package com.bootcamp.tugas3_bootcampidn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bootcamp.tugas3_bootcampidn.databinding.ActivityDetailNewsBinding
+import com.bootcamp.tugas3_bootcampidn.model.ArticlesItem
+import com.bumptech.glide.Glide
 
 class DetailNewsActivity : AppCompatActivity() {
+
+	private lateinit var binding: ActivityDetailNewsBinding
 
 	companion object{
 		const val EXTRA_NEWS = "extra_news"
@@ -13,6 +18,19 @@ class DetailNewsActivity : AppCompatActivity() {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_detail_news)
 
+		binding = ActivityDetailNewsBinding.inflate(layoutInflater)
 
+		supportActionBar?.title="Detail"
+		val dataNews = intent.getParcelableExtra<ArticlesItem>(EXTRA_NEWS)
+
+		binding.apply {
+			tvJudul.text = dataNews?.title
+			tvDeskripsi.text = dataNews?.description
+
+			Glide.with(this@DetailNewsActivity)
+				.load(dataNews?.urlToImage)
+				.error(R.drawable.ic_launcher_background)
+				.into(imgNews)
+		}
 	}
 }
